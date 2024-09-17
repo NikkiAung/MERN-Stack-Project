c29c219507efcdc46b333cf68857175063e61bea
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-09-16 18:47:45 -0700

    develop:connected to MogoDB successfully

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
 let morgan = require('morgan')
+const mongoose = require('mongoose')
 require('dotenv').config()
 const recipesRoutes = require('./routes/recipes');
 app.use(morgan('dev'));
-
+let mongoURL = "mongodb+srv://anandaooit:test1234@mern-cluster.bl5c9.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Cluster"
+mongoose.connect(mongoURL).then(()=> {
+    console.log('connected to db');
+    app.listen(process.env.PORT, ()=> {
+        console.log('Server starts listening on localhost:'+process.env.PORT);
+    })
+})
 app.get('/', (req, res)=> {
     return res.json({hello:'hello'})
 })
 
 app.use('/api/recipes', recipesRoutes);
 
-app.listen(process.env.PORT, ()=> {
-    console.log('Server starts listening on localhost:'+process.env.PORT);
-})
