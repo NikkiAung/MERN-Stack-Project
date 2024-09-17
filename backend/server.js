32756b114c00b1969442874be20b64d6ad8e00e6
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-09-16 17:07:07 -0700

    develop:setting up recipe router & its controller

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 let morgan = require('morgan')
 require('dotenv').config()
-
+const recipesRoutes = require('./routes/recipes');
 app.use(morgan('dev'));
 
 app.get('/', (req, res)=> {
     return res.json({hello:'hello'})
 })
+
+app.use('/api/recipes', recipesRoutes);
 
 app.listen(process.env.PORT, ()=> {
     console.log('Server starts listening on localhost:'+process.env.PORT);
