bdd3f8d53c5753f135bbb7f91a0f4ed79243008c
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-09-21 16:17:40 -0700

    develop:init cookie set-up

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const recipesRoutes = require('./routes/recipes');
 const userRoutes = require('./routes/users')
+let cookieParser = require('cookie-parser')
 
 let mongoURL = "mongodb+srv://anandaooit:test1234@mern-cluster.bl5c9.mongodb.net/?retryWrites=true&w=majority&appName=MERN-Cluster"
 mongoose.connect(mongoURL).then(()=> {
@@ -18,9 +19,21 @@ mongoose.connect(mongoURL).then(()=> {
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.get('/', (req, res)=> {
     return res.json({hello:'hello'})
 })
 
 app.use('/api/recipes', recipesRoutes);
 app.use('/api/users', userRoutes);
+
+app.get('/set-cookie',(req,res)=>{
+    res.cookie('name','Aung');
+    res.cookie('note','important',{httpOnly:true});
+    return res.send('Cookie alr set!')
+})
+
+app.get('/get-cookie',(req, res)=>{
+    const cookies = req.cookies;
+    return res.json(cookies);
+})
