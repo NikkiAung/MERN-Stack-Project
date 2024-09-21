e82e74d34d911a5df1f186e618a294c1699b72a6
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-09-21 00:41:50 -0700

    develop:register controller + hash password with bcrypt

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,15 +1,30 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 const UserController = {
     login : (req, res) =>{
         return res.json({msg:
             'user login api hit'
         });
     },
-    register : (req, res) => {
-        return res.json({msg:
-            'user register api hit'
-        })
+    register : async(req, res) => {
+        try {
+            const {name, email, password} = req.body;
+            const userExists = await User.findOne({email});
+            if(userExists){
+                throw new Error('User already exists!')
+            }
+            const salt = await bcrypt.genSalt();
+            const hashValue = await bcrypt.hash(password,salt);
+            const user = await User.create({
+                name, 
+                email,
+                password : hashValue
+            });
+            return res.json(user);
+        } catch (e) {
+            return res.status(400).json({error : e.message});
+        }
     }
-
 }
 
 module.exports = UserController;
