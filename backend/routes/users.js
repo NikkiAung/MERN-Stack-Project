695244a2637233ecf0b0beb4a6126398c74cfe98
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-09-22 17:20:59 -0700

    update:email custome express valid @ users.js and not giving access to JS by adding httpOnly @ JWT

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,11 +2,18 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/UserController');
 const { body } = require('express-validator');
-const errorMessagehanlder = require('../Middleware/handleErrorMessage')
+const errorMessagehanlder = require('../Middleware/handleErrorMessage');
+const User = require('../models/User');
 router.post('/login', UserController.login);
 router.post('/register', [
         body('name').notEmpty(),
         body('email').notEmpty(),
+        body('email').custom(async value => {
+            const user = await User.findOne({email : value});
+            if (user) {
+              throw new Error('E-mail already in use');
+            }
+        }),
         body('password').notEmpty(),
     ],errorMessagehanlder,UserController.register);
 
