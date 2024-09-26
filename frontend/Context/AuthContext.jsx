a20b903d666ac5ee45088f50474de68f352a6bc5
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-09-25 19:39:20 -0700

    develop:localStorage to remember user login

--- frontend/Context/AuthContext.jsx
+++ frontend/Context/AuthContext.jsx
@@ -1,12 +1,14 @@
-import { createContext, useReducer } from "react";
+import { createContext, useEffect, useReducer } from "react";
 
 const AuthContext = createContext();
 
 const AuthReducer = (state, action) => {
     switch (action.type) {
         case "LOGIN":
+            localStorage.setItem('user',JSON.stringify(action.payload));
             return { user : action.payload };
         case "LOGOUT":
+            localStorage.removeItem('user');
             return { user : null };
         default:
             return { state };
@@ -18,6 +20,19 @@ const AuthContextProvider = ({children}) => {
     const [state,dispatch] = useReducer(AuthReducer,{
         user: null
     });
+    useEffect(()=>{
+        try {
+            const user = JSON.parse(localStorage.getItem('user'));
+            console.log(user);
+            if(user) {
+                dispatch({type:"LOGIN",payload:user})
+            }else{
+                dispatch({type:"LOGOUT"})
+            }
+        } catch (e) {
+            dispatch({ type: "LOGOUT" });
+        }
+    },[])
     return( 
         <AuthContext.Provider value={{...state,dispatch}}>
             {children}
