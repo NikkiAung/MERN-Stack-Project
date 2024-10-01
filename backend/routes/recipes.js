07abf241da8adee0ad0ebebae3225ada72f174b4
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-09-30 23:03:21 -0700

    develop:try and catch err for photo upload

--- backend/routes/recipes.js
+++ backend/routes/recipes.js
@@ -11,7 +11,19 @@ router.post('', [
                     body('ingredients').notEmpty().isArray({min:3})
                 ], errorMessagehanlder , RecipeController.store);
 router.get('/:id', RecipeController.show);
-router.post('/:id/upload',upload.single('photo'),RecipeController.upload)
+router.post('/:id/upload',[
+    upload.single('photo'),
+    body('photo').custom((value,{req})=> {
+        if(!req.file){
+            throw new Error("Photo is required");
+        }
+        if(!req.file.mimetype.startsWith('image')){
+            throw new Error("Photo must be image");
+        }
+        return true
+    })
+
+],errorMessagehanlder,RecipeController.upload)
 router.delete('/:id', RecipeController.destroy);
 router.patch('/:id', RecipeController.update);
 
